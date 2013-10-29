889c9be290f7d1edce7103d880193b0063c09ae3
David Haynes <david.e.haynes@stfc.ac.uk>
2013-10-29 11:00:57 +0000

    Added some tests for EmailCredential (failing)

--- cloudhands/common/schema.py
+++ cloudhands/common/schema.py
@@ -23,7 +23,80 @@ The schema module defines tables in the common database
 """
 
 
+class Artifact(Base):
+    __tablename__ = "artifacts"
+
+    id = Column("id", Integer(), nullable=False, primary_key=True)
+    typ = Column("typ", String(length=32), nullable=False)
+    uuid = Column("uuid", CHAR(length=32), nullable=False)
+    model = Column("model", String(length=32), nullable=False)
+    changes = relationship("Touch")
+
+    __mapper_args__ = {
+        "polymorphic_identity": "artifact",
+        "polymorphic_on": typ}
+
+
+class EmailCredential(Artifact):
+    __tablename__ = "emailcredentials"
+
+    id = Column("id", Integer, ForeignKey("artifacts.id"),
+                nullable=False, primary_key=True)
+    email = Column("email", String(length=128), nullable=False, unique=True)
+
+    __mapper_args__ = {"polymorphic_identity": "emailcredential"}
+
+
+class Actor(Base):
+    """
+    This is the base table for all actors in the system. Concrete classes
+    define their own tables according to SQLAlchemy's
+    `joined-table inheritance`_.
+
+    .. _joined-table inheritance: http://docs.sqlalchemy.org/en/latest/orm\
+/inheritance.html#joined-table-inheritance
+    """
+
+    __tablename__ = "actors"
+
+    id = Column("id", Integer(), nullable=False, primary_key=True)
+    typ = Column("typ", String(length=32), nullable=False)
+    uuid = Column("uuid", CHAR(length=32), nullable=False, unique=True)
+    handle = Column("handle", String(length=64), nullable=True, unique=True)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "actor",
+        "polymorphic_on": typ}
+
+
+class User(Actor):
+    __tablename__ = "users"
+
+    id = Column("id", Integer, ForeignKey("actors.id"),
+                nullable=False, primary_key=True)
+
+    __mapper_args__ = {"polymorphic_identity": "user"}
+
+
+class Touch(Base):
+    __tablename__ = "touches"
+
+    id = Column("id", Integer(), nullable=False, primary_key=True)
+    artifact_id = Column("artifact_id", Integer, ForeignKey("artifacts.id"))
+    actor_id = Column("actor_id", Integer, ForeignKey("actors.id"))
+    state_id = Column("state_id", Integer, ForeignKey("states.id"))
+    at = Column("at", DateTime(), nullable=False)
+
+    artifact = relationship("Artifact")
+    actor = relationship("Actor")
+    state = relationship("State")
+
+
 class State(Base):
+    """
+    State machines which persist their state in the database declare themselves
+    using this table.
+    """
     __tablename__ = "states"
 
     __table_args__ = (UniqueConstraint("fsm", "name"),)
@@ -36,10 +109,20 @@ class State(Base):
 
 
 def state_init(class_, fsm, states):
+    """
+    A generic initialiser for a state object.
+    """
     return class_(fsm=fsm, name=states[0])
 
 
 def fsm_factory(name, states):
+    """
+    Dynamically create a class for a state machine. The pattern used
+    is SQLAlchemy's `single table inheritance`_.
+
+    .. _single table inheritance: http://docs.sqlalchemy.org/en/latest/orm\
+/inheritance.html#single-table-inheritance
+    """
     className = name.capitalize() + "State"
     attribs = dict(
         __mapper_args__={"polymorphic_identity": name},
@@ -50,4 +133,5 @@ def fsm_factory(name, states):
     class_.init = functools.partial(state_init, class_, name, states)
     return class_
 
-CredentialState = fsm_factory("credential", ["untrusted", "trusted", "expired"])
+CredentialState = fsm_factory(
+    "credential", ["untrusted", "trusted", "expired"])
--- cloudhands/common/test_schema.py
+++ cloudhands/common/test_schema.py
@@ -1,18 +1,153 @@
 #!/usr/bin/env python3
 #   encoding: UTF-8
 
+import datetime
 import unittest
+import sqlite3
 
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
+from cloudhands.common.schema import metadata
 from cloudhands.common.schema import CredentialState
+from cloudhands.common.schema import EmailCredential
+from cloudhands.common.schema import State
 
-class TestCredentialState(unittest.TestCase):
+Session = sessionmaker()
+
+class SQLite3Client(object):
+    """
+    A mixin class which sets up a connection to a SQLite3 database
+    and binds it through SQLAlchemy to this module's Session class.
+    """
+
+    @staticmethod
+    def sqlite_fk_pragma(dbapi_con, con_record):
+        dbapi_con.execute("pragma foreign_keys=ON")
+
+    @staticmethod
+    def on_connect(dbapi_con, con_record):
+        SQLite3Client.sqlite_fk_pragma(dbapi_con, con_record)
+
+    def connect(self, module, path=":memory:"):
+        """
+        Creates, configures and returns a SQLAlchemy engine connected
+        to a SQLite3 database.
+        """
+        #TODO: use sqlalchemy.engine.url.URL
+        sqlaPath = "sqlite:///" + path
+        engine = sqlalchemy.create_engine(
+                    sqlaPath, module=module, poolclass=StaticPool,
+                    connect_args={"check_same_thread": False})
+        sqlalchemy.event.listen(engine, "connect", self.on_connect)
+        metadata.bind = engine
+        metadata.create_all()
+        Session.configure(bind=engine)
+        return engine
+
+
+class TestCredentialState(SQLite3Client, unittest.TestCase):
+
+    def setUp(self):
+        """ Every test gets its own in-memory database """
+        self.engine = self.connect(sqlite3)
 
     def test_initialisation(self):
         obj = CredentialState.init()
         self.assertEqual(CredentialState.table, obj.fsm)
         self.assertEqual(CredentialState.values[0], obj.name)
-        print(vars(obj))
+
+
+    def test_duplicate_names(self):
+        session = Session()
+        session.add_all([
+            State(fsm="credential", name="untrusted"),
+            State(fsm="credential", name="untrusted")])
+        self.assertRaises(
+            sqlalchemy.exc.IntegrityError, session.commit)
+
+    def test_shared_names(self):
+        """ State names can be shared across FSMs """
+        session = Session()
+        session.add_all([
+            State(fsm="credential", name="start"),
+            State(fsm="billing", name="start")])
+        try:
+            session.commit()
+        except sqlalchemy.exc.IntegrityError as e:
+            self.fail(e)
+
+
+class TestEmailCredentialFSM(SQLite3Client, unittest.TestCase):
+
+    def setUp(self):
+        """ Every test gets its own in-memory database """
+        self.engine = self.connect(sqlite3)
+
+
+    def test_create(self):
+        session = Session()
+        self.assertRaises(TypeError, EmailCredential.init)
+
+    def test_transitions(self):
+        cred = EmailCredential.init("somebody@gmail.com")
+
+    def test_new_touch(self):
+        then = datetime.datetime.utcnow() - datetime.timedelta(seconds=1)
+        session = Session()
+        u = User(
+            handle="Anon", email="anonymous@gmail.com",
+            uuid=uuid.uuid4().hex)
+        start = State(fsm="lifecycle", name="start")
+        stop = State(fsm="lifecycle", name="stop")
+        f = Pricing(
+            name="standard", value=5, currency="GBP",
+            commission_num=1, commission_den=1,
+            description="description")
+        p = Product(
+            typ="product", uuid=uuid.uuid4().hex,
+            model=topicmob.schema.__version__,
+            title="title", description="descr", min=4, max=10,
+            fee=f)
+        p.changes.append(
+            Touch(artifact=p, actor=u, state=start, at=then))
+        session.add(p)
+        session.commit()
+
+        self.assertIs(p.changes[0].state, start)
+        self.assertIs(session.query(Touch).first().state, start)
+        self.assertEqual(session.query(Touch).count(), 1)
+
+        now = datetime.datetime.utcnow()
+        self.assertTrue(now > then)
+        p.changes.append(
+            Touch(artifact=p, actor=u, state=stop, at=now))
+        session.commit()
+
+        self.assertIs(p.changes[1].state, stop)
+        self.assertIs(
+            session.query(Touch).order_by(Touch.at)[-1].state, stop)
+        self.assertEqual(session.query(Touch).count(), 2)
+
+        self.assertEqual(
+            session.query(Touch).filter(Touch.at < now).first(),
+            p.changes[0])
+        self.assertIs(
+            session.query(Touch).filter(
+                Touch.at > then).first(),
+            p.changes[1])
+
+        p.changes.sort(key=operator.attrgetter("at"), reverse=True)
+
+        self.assertEqual(
+            session.query(Touch).filter(
+                Touch.at < now).first(),
+            p.changes[1])
+        self.assertIs(
+            session.query(Touch).filter(
+                Touch.at > then).first(),
+            p.changes[0])
 
 if __name__ == "__main__":
     unittest.main()
