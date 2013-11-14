6364d81c0a74fbcb7e583fdc6058fa186f9d137f
David Haynes <david.e.haynes@stfc.ac.uk>
2013-11-14 10:38:22 +0000

    Populated schema tricks module, plus simple test

---
+++ cloudhands/common/test/test_tricks.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+import sqlite3
+import unittest
+
+from cloudhands.common.connectors import SQLite3Client
+from cloudhands.common.connectors import Session
+
+from cloudhands.common.fsm import MembershipState
+
+import cloudhands.common.schema
+from cloudhands.common.schema import EmailAddress
+from cloudhands.common.schema import Membership
+from cloudhands.common.schema import State
+from cloudhands.common.schema import Touch
+from cloudhands.common.schema import User
+
+from cloudhands.common.tricks import create_user_grant_email_membership
+
+
+class TestUserMembership(SQLite3Client, unittest.TestCase):
+
+    def setUp(self):
+        """ Every test gets its own in-memory database """
+        self.engine = self.connect(sqlite3)
+        session = Session()
+        session.add_all(
+            State(fsm=MembershipState.table, name=v)
+            for v in MembershipState.values)
+        session.commit()
+
+    def test_quick_add_user(self):
+        session = Session()
+        val = "my.name@test.org"
+        user = create_user_grant_email_membership(session, val)
--- cloudhands/common/tricks.py
+++ cloudhands/common/tricks.py
@@ -1,18 +1,66 @@
 #!/usr/bin/env python3
 #   encoding: UTF-8
 
+import datetime
+import logging
+import uuid
+
+from cloudhands.common.fsm import MembershipState
+
+import cloudhands.common.schema
+from cloudhands.common.schema import EmailAddress
+from cloudhands.common.schema import Membership
+from cloudhands.common.schema import Resource
+from cloudhands.common.schema import State
+from cloudhands.common.schema import Touch
+from cloudhands.common.schema import User
+
 __doc__ = """
-Helper functions and factories.
+Common functions for interacting with the schema.
 """
 
-#_FSMP = namedtuple("FSMParameters", ["table", "states"])
-#
-#def state_init(class_, params):
-#    return class_(fsm=params.table, name=params.states[0])
-#
-#
-#def fsm_factory(name, states):
-#    className = name.capitalize() + "State"
-#    attribs = dict(__mapper_args__={"polymorphic_identity": name})
-#    class_ = type(className, (State,), attribs)
-#    return (_FSMP(name, states), class_)
+def create_user_grant_email_membership(
+    session, addrVal, handle=None, org=None, role="user"):
+    """
+    Creates a new user account from an email address. The sequence of
+    operations is:
+
+    1.  Create a User record.
+    2.  Add a new Membership record.
+    3.  Touch the membership with an EmailAddress resource from the user.
+        The state of membership is set to `granted`.
+
+    :param session:  A SQLALchemy database session.
+    :param string addrVal:   The user's email address.
+    :param string handle:   Becomes the user's handle. If not supplied, 
+                            handle is constructed from the `addrVal`.
+    :param string org: Membership organisation.
+    :param string role: Membership role.
+    :returns: The newly created User object.
+    """
+
+    # 1.
+    handle = handle or ' '.join(
+        i.capitalize() for i in addrVal.split('@')[0].split('.'))
+    user = User(handle=handle, uuid=uuid.uuid4().hex)
+
+    # 2.
+    mship = Membership(
+        uuid=uuid.uuid4().hex,
+        model=cloudhands.common.__version__,
+        organisation=org,
+        role=role)
+    session.add(mship)
+    session.commit()
+
+    # 3.
+    granted = session.query(
+        MembershipState).filter(MembershipState.name == "granted").one()
+    now = datetime.datetime.utcnow()
+
+    grant = Touch(artifact=mship, actor=user, state=granted, at=now)
+    mship.changes.append(grant)
+    ea = EmailAddress(value=addrVal, touch=grant)
+    session.add(ea)
+    session.commit()
+    return user
