85b36522d028debeb94076f6203a6942b91a9734
David Haynes <david.e.haynes@stfc.ac.uk>
2014-03-14 12:07:09 +0000

    Added test for Registration touch with BcryptedPassword resource

--- cloudhands/common/test/test_registration.py
+++ cloudhands/common/test/test_registration.py
@@ -2,9 +2,12 @@
 #   encoding: UTF-8
 
 import datetime
+import operator
 import sqlite3
 import unittest
 import uuid
+
+import cloudhands.common
 
 from cloudhands.common.connectors import initialise
 from cloudhands.common.connectors import Registry
@@ -32,5 +35,58 @@ class RegistrationTests(unittest.TestCase):
         r = Registry()
         r.disconnect(sqlite3, ":memory:")
 
-    def test_fisrst(self):
-        self.fail()
+    def test_using_touches(self):
+        then = datetime.datetime.utcnow() - datetime.timedelta(seconds=1)
+        session = Registry().connect(sqlite3, ":memory:").session
+
+        user = User(handle=None, uuid=uuid.uuid4().hex)
+
+        reg = Registration(
+            uuid=uuid.uuid4().hex,
+            model=cloudhands.common.__version__)
+
+        prepass = session.query(RegistrationState).filter(
+            RegistrationState.name == "prepass").one()
+        reg.changes.append(
+            Touch(artifact=reg, actor=user, state=prepass, at=then))
+        session.add(reg)
+        session.commit()
+
+        self.assertIs(reg.changes[0].state, prepass)
+        self.assertIs(session.query(Touch).first().state, prepass)
+        self.assertEqual(session.query(Touch).count(), 1)
+
+        now = datetime.datetime.utcnow()
+        self.assertTrue(now > then)
+        valid = session.query(RegistrationState).filter(
+            RegistrationState.name == "valid").one()
+        act = Touch(artifact=reg, actor=user, state=valid, at=now)
+        reg.changes.append(act)
+        hash = BcryptedPassword(value="a" * 60, touch=act, provider=None)
+        session.add(hash)
+        session.commit()
+
+        self.assertIs(reg.changes[1].state, valid)
+        self.assertIs(
+            session.query(Touch).order_by(Touch.at)[-1].state, valid)
+        self.assertEqual(session.query(Touch).count(), 2)
+
+        self.assertEqual(
+            session.query(Touch).filter(Touch.at < now).first(),
+            reg.changes[0])
+        self.assertIs(
+            session.query(Touch).filter(
+                Touch.at > then).first(),
+            reg.changes[1])
+
+        reg.changes.sort(key=operator.attrgetter("at"), reverse=True)
+
+        self.assertEqual(
+            session.query(Touch).filter(
+                Touch.at < now).first(),
+            reg.changes[1])
+        self.assertIs(
+            session.query(Touch).filter(
+                Touch.at > then).first(),
+            reg.changes[0])
+
