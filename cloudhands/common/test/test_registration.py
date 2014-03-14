2c2a346a102d7eaa32696d0949ffa05e31e6ce3c
David Haynes <david.e.haynes@stfc.ac.uk>
2014-03-14 09:22:54 +0000

    Added tests for registration in schema

---
+++ cloudhands/common/test/test_registration.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#   encoding: UTF-8
+
+import unittest
+
+from cloudhands.common.schema import Registration
+from cloudhands.common.schema import Touch
+from cloudhands.common.schema import User
+
+class RegistrationTests(unittest.TestCase):
+
+    def first_test(self):
+        self.fail()
