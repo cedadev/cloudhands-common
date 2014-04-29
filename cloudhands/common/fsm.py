4c4a31e65aadfdd597433bed0b8e4fcaba82fe3a
David Haynes <david.e.haynes@stfc.ac.uk>
2014-04-29 12:39:25 +0100

    Sketching out new state machine for Registration

--- cloudhands/common/fsm.py
+++ cloudhands/common/fsm.py
@@ -12,5 +12,15 @@ MembershipState = fsm_factory(
 RegistrationState = fsm_factory(
     "registration", ["modified", "preconfirm", "postconfirm", "valid", "expired", "withdrawn"])
 
+RegistrationState = fsm_factory(
+    "registration", [
+        "pre_registration_person",
+        "pre_registration_inetorgperson",
+        "pre_registration_inetorgperson_sn",
+        "pre_user_inetorgperson_dn",
+        "pre_user_posixaccount",
+        "pre_user_ldappublickey",
+])
+
 SubscriptionState = fsm_factory(
     "subscription", ["maintenance", "unchecked", "inactive", "active"])
