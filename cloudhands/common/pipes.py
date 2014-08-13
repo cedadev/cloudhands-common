755464f7f619dd1a003c982cbb2e35e912360e5e
David Haynes <david.e.haynes@stfc.ac.uk>
2014-08-13 14:44:24 +0100

    Tidied up PipeQueue code. Separate test module. Upversion.

--- cloudhands/common/pipes.py
+++ cloudhands/common/pipes.py
@@ -1,89 +1,16 @@
 #!/usr/bin/env python3
 #   encoding: UTF-8
 
-import argparse
 import ast
 import asyncio
-from collections import namedtuple
-from contextlib import ExitStack
-import logging
 import os
 from pprint import pprint
 import sys
 
-Connection = namedtuple("Connection", ["module", "path", "engine", "session"])
-
 __doc__ = """
-Spike for message passing over asyncio-wrapped named pipes.
+Provides an interprocess Queue for use with the asyncio event loop.
 """
 
-# prototyping
-import unittest
-
-class PipeQueueTest(unittest.TestCase):
-
-    def setUp(self):
-        self.path = "test.fifo"
-        try:
-            os.remove(self.path)
-        except FileNotFoundError:
-            pass
-
-    def tearDown(self):
-        self.setUp()
-
-    def test_no_history(self):
-        os.mkfifo(self.path)
-        self.assertRaises(
-            FileExistsError,
-            PipeQueue(self.path, history=False).__enter__)
-
-    def test_attributes(self):
-        with PipeQueue(self.path) as pq:
-            self.assertIsNotNone(pq._in)
-            self.assertIsNotNone(pq._out)
-            self.assertTrue(hasattr(pq, "get"))
-            self.assertTrue(hasattr(pq, "put"))
-
-    def test_simple_read_write(self):
-        loop = asyncio.get_event_loop()
-        with PipeQueue(self.path) as pq:
-            loop.run_until_complete(pq.put("S"))
-            rv = loop.run_until_complete(pq.get())
-            self.assertEqual("S", rv)
-
-    def test_tuple_read_write(self):
-        loop = asyncio.get_event_loop()
-        payload = (12, "string")
-        with PipeQueue(self.path) as pq:
-            loop.run_until_complete(pq.put(payload))
-            rv = loop.run_until_complete(pq.get())
-            self.assertEqual(payload, rv)
-
-    def test_multiple_read_write(self):
-        loop = asyncio.get_event_loop()
-        payloads = ((i, "string") for i in range(6))
-        with PipeQueue(self.path) as pq:
-            for n, payload in enumerate(payloads):
-                loop.run_until_complete(pq.put(payload))
-
-            for i in range(n):
-                rv = loop.run_until_complete(pq.get())
-                self.assertIsInstance(rv, tuple)
-                self.assertEqual(i, rv[0])
-                self.assertEqual("string", rv[1])
-
-    def test_queue_returned_by_factory(self):
-
-        def factory():
-            return PipeQueue.pipequeue(self.path)
-
-        loop = asyncio.get_event_loop()
-        pq = factory()
-        loop.run_until_complete(pq.put("S"))
-        rv = loop.run_until_complete(pq.get())
-        self.assertEqual("S", rv)
-        pq.close()
 
 class PipeQueue:
 
@@ -135,54 +62,3 @@ class PipeQueue:
     def close(self): 
         self._out.close()
         self._in.close()
-
-def main(args):
-    path, = args.path
-    try:
-        os.mkfifo(path)
-    except FileExistsError:
-        pass
-
-    loop = asyncio.get_event_loop()
-    future = asyncio.Future()
-    asyncio.Task(wait_until_open_for_write(future, path))
-    future.add_done_callback(pipe_open_for_write)
-
-    try:
-        fd = os.open(path, os.O_RDONLY | os.O_NONBLOCK)
-        loop.run_forever()
-    finally:
-        loop.close()
-        os.close(fd)
-        os.remove(path)
-    return 1
-
-def parser():
-    rv = argparse.ArgumentParser(description=__doc__)
-    rv.add_argument(
-        "--version", action="store_true", default=False,
-        help="Print the current version number")
-    rv.add_argument(
-        "-v", "--verbose", required=False,
-        action="store_const", dest="log_level",
-        const=logging.DEBUG, default=logging.INFO,
-        help="Increase the verbosity of output")
-    rv.add_argument(
-        "--log", default=None, dest="log_path",
-        help="Set a file path for log output")
-    rv.add_argument(
-        "path", default=None, nargs=1,
-        help="Set the path to the named pipe")
-    return rv
-
-
-def run():
-    p = parser()
-    args = p.parse_args()
-    rv = main(args)
-    return rv
-
-
-if __name__ == "__main__":
-    unittest.main()
-    sys.exit(run())
---
+++ cloudhands/common/test/test_pipes.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+#   encoding: UTF-8
+
+import asyncio
+import os
+import unittest
+
+from cloudhands.common.pipes import PipeQueue
+
+
+class PipeQueueTest(unittest.TestCase):
+
+    def setUp(self):
+        self.path = "test.fifo"
+        try:
+            os.remove(self.path)
+        except FileNotFoundError:
+            pass
+
+    def tearDown(self):
+        self.setUp()
+
+    def test_no_history(self):
+        os.mkfifo(self.path)
+        self.assertRaises(
+            FileExistsError,
+            PipeQueue(self.path, history=False).__enter__)
+
+    def test_attributes(self):
+        with PipeQueue(self.path) as pq:
+            self.assertIsNotNone(pq._in)
+            self.assertIsNotNone(pq._out)
+            self.assertTrue(hasattr(pq, "get"))
+            self.assertTrue(hasattr(pq, "put"))
+
+    def test_simple_read_write(self):
+        loop = asyncio.get_event_loop()
+        with PipeQueue(self.path) as pq:
+            loop.run_until_complete(pq.put("S"))
+            rv = loop.run_until_complete(pq.get())
+            self.assertEqual("S", rv)
+
+    def test_tuple_read_write(self):
+        loop = asyncio.get_event_loop()
+        payload = (12, "string")
+        with PipeQueue(self.path) as pq:
+            loop.run_until_complete(pq.put(payload))
+            rv = loop.run_until_complete(pq.get())
+            self.assertEqual(payload, rv)
+
+    def test_multiple_read_write(self):
+        loop = asyncio.get_event_loop()
+        payloads = ((i, "string") for i in range(6))
+        with PipeQueue(self.path) as pq:
+            for n, payload in enumerate(payloads):
+                loop.run_until_complete(pq.put(payload))
+
+            for i in range(n):
+                rv = loop.run_until_complete(pq.get())
+                self.assertIsInstance(rv, tuple)
+                self.assertEqual(i, rv[0])
+                self.assertEqual("string", rv[1])
+
+    def test_queue_returned_by_factory(self):
+
+        def factory():
+            return PipeQueue.pipequeue(self.path)
+
+        loop = asyncio.get_event_loop()
+        pq = factory()
+        loop.run_until_complete(pq.put("S"))
+        rv = loop.run_until_complete(pq.get())
+        self.assertEqual("S", rv)
+        pq.close()
