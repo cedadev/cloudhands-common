2e62507fb1632226f01c5b4e0d09ab2233982f1c
David Haynes <david.e.haynes@stfc.ac.uk>
2014-08-13 12:01:27 +0100

    Tests pass for PipeQueue code.

--- cloudhands/common/pipes.py
+++ cloudhands/common/pipes.py
@@ -2,10 +2,13 @@
 #   encoding: UTF-8
 
 import argparse
+import ast
 import asyncio
 from collections import namedtuple
+from contextlib import ExitStack
 import logging
 import os
+from pprint import pprint
 import sys
 
 Connection = namedtuple("Connection", ["module", "path", "engine", "session"])
@@ -19,43 +22,120 @@ import unittest
 
 class PipeQueueTest(unittest.TestCase):
 
+    def setUp(self):
+        self.path = "test.fifo"
+        try:
+            os.remove(self.path)
+        except FileNotFoundError:
+            pass
+
+    def tearDown(self):
+        self.setUp()
+
+    def test_no_history(self):
+        os.mkfifo(self.path)
+        self.assertRaises(
+            FileExistsError,
+            PipeQueue(self.path, history=False).__enter__)
+
     def test_attributes(self):
-        with PipeQueue("test.fifo") as pq:
-            self.assertTrue(hasattr(pq, "node"))
+        with PipeQueue(self.path) as pq:
+            self.assertIsNotNone(pq._in)
+            self.assertIsNotNone(pq._out)
+            self.assertTrue(hasattr(pq, "get"))
+            self.assertTrue(hasattr(pq, "put"))
+
+    def test_simple_read_write(self):
+        loop = asyncio.get_event_loop()
+        with PipeQueue(self.path) as pq:
+            loop.run_until_complete(pq.put("S"))
+            rv = loop.run_until_complete(pq.get())
+            self.assertEqual("S", rv)
+
+    def test_tuple_read_write(self):
+        loop = asyncio.get_event_loop()
+        payload = (12, "string")
+        with PipeQueue(self.path) as pq:
+            loop.run_until_complete(pq.put(payload))
+            rv = loop.run_until_complete(pq.get())
+            self.assertEqual(payload, rv)
+
+    def test_multiple_read_write(self):
+        loop = asyncio.get_event_loop()
+        payloads = ((i, "string") for i in range(6))
+        with PipeQueue(self.path) as pq:
+            for n, payload in enumerate(payloads):
+                loop.run_until_complete(pq.put(payload))
+
+            for i in range(n):
+                rv = loop.run_until_complete(pq.get())
+                self.assertIsInstance(rv, tuple)
+                self.assertEqual(i, rv[0])
+                self.assertEqual("string", rv[1])
+
+    def test_queue_returned_by_factory(self):
+
+        def factory():
+            return PipeQueue.pipequeue(self.path)
+
+        loop = asyncio.get_event_loop()
+        pq = factory()
+        loop.run_until_complete(pq.put("S"))
+        rv = loop.run_until_complete(pq.get())
+        self.assertEqual("S", rv)
+        pq.close()
 
 class PipeQueue:
 
-    def __init__(self, path=None):
+    @staticmethod
+    def pipequeue(*args, **kwargs):
+        return PipeQueue(*args, **kwargs).__enter__()
+
+    def __init__(self, path, history=True):
         self.path = path
-        self.fd = None
+        self.history = history
 
     def __enter__(self):
         try:
             os.mkfifo(self.path)
         except FileExistsError:
-            pass
+            if not self.history:
+                raise
 
-        loop = asyncio.get_event_loop()
-        future = asyncio.Future()
-        future.add_done_callback(self.pipe_open_for_write)
-
-        self.fd = os.open(self.path, os.O_RDONLY | os.O_NONBLOCK)
-        loop.run_until_complete(
-            self.wait_until_open_for_write(future, self.path))
+        self._out = os.fdopen(
+            os.open(self.path, os.O_RDONLY | os.O_NONBLOCK),
+            buffering=1,
+            encoding="utf-8")
+        self._in = open(self.path, "w", buffering=1, encoding="utf-8")
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        os.close(self.fd)
-        os.remove(self.path)
+        self.close()
+        if not self.history:
+            os.remove(self.path)
         return False
 
     @asyncio.coroutine
-    def wait_until_open_for_write(self, future, path):
-        future.set_result(open(path, 'w'))
+    def get(self):
+        future = asyncio.Future()
+        payload = self._out.readline().rstrip("\n")
+        future.set_result(ast.literal_eval(payload))
+        return future 
+
+    @asyncio.coroutine
+    def put(self, msg):
+        future = asyncio.Future()
+        try:
+            pprint(msg, stream=self._in, compact=True, width=sys.maxsize)
+        except TypeError:  # 'compact' is new in Python 3.4
+            pprint(msg, stream=self._in, width=sys.maxsize)
+        future.set_result(msg)
         return future
 
-    def pipe_open_for_write(self, future):
-        print("Yay! ", future.result())
-        
+    def close(self): 
+        self._out.close()
+        self._in.close()
+
 def main(args):
     path, = args.path
     try:
