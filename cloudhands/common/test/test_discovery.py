c94b3c0362e4b50073965654f47735b0d81a0e18
Charles Llewellyn <charlie.j.llwellyn@gmail.com>
2014-09-30 09:18:06 +0100

    Temporary test code during integration.

--- cloudhands/common/test/test_discovery.py
+++ cloudhands/common/test/test_discovery.py
@@ -5,6 +5,7 @@ import unittest
 
 import cloudhands.common.states
 from cloudhands.common.discovery import fsms
+from cloudhands.common.discovery import providers
 
 
 class DiscoveryTest(unittest.TestCase):
@@ -12,3 +13,9 @@ class DiscoveryTest(unittest.TestCase):
     def test_state_machines(self):
         self.assertIn(cloudhands.common.states.SubscriptionState, fsms)
         self.assertIn(cloudhands.common.states.HostState, fsms)
+    
+    def test_provider_config(self):
+        configs = {cfg["metadata"]["path"]: cfg
+                  for p in providers.values() for cfg in p}
+        config = configs["cloudhands.jasmin.vcloud.ref-portalTest-U.cfg"]
+        print(config["host"]["name"])
