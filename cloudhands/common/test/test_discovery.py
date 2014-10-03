3775c2154913dcaf70324f31432246d7c78a69e0
David Haynes <david.e.haynes@stfc.ac.uk>
2014-10-03 09:32:07 +0100

    Removed temporary test made during integration.

--- cloudhands/common/test/test_discovery.py
+++ cloudhands/common/test/test_discovery.py
@@ -13,9 +13,3 @@ class DiscoveryTest(unittest.TestCase):
     def test_state_machines(self):
         self.assertIn(cloudhands.common.states.SubscriptionState, fsms)
         self.assertIn(cloudhands.common.states.HostState, fsms)
-    
-    def test_provider_config(self):
-        configs = {cfg["metadata"]["path"]: cfg
-                  for p in providers.values() for cfg in p}
-        config = configs["cloudhands.jasmin.vcloud.ref-portalTest-U.cfg"]
-        print(config["host"]["name"])
